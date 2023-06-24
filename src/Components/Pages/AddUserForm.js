2e196b2bd659a83fefd79e554e9463810428b8ea
krunalbadgujar311 <82201503+krunalbadgujar311@users.noreply.github.com>
2023-06-24 10:19:27 +0530

    Update AddUserForm.js

--- src/Components/Pages/AddUserForm.js
+++ src/Components/Pages/AddUserForm.js
@@ -1,56 +1,109 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-function RegistrationForm() {
-  const [formData, setFormData] = useState({
-    name: '',
-    email: '',
-    password: ''
-  });
+import React from 'react';
+import { Formik, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+import './RegistrationForm.css'; // Import your CSS file for styling
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData((prevData) => ({
-      ...prevData,
-      [name]: value
-    }));
+const AddUserForm = () => {
+
+  const initialValues = {
+    empid:'',
+    firstName: '',
+    lastName: '',
+    age: 18,
+    visits: 0,
+    progress:0,
+    status: 0
   };
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      const response = await axios.post('/api/adduser', formData);
-      console.log(response.data);
-      // Reset the form after successful submission
-      setFormData({
-        name: '',
-        email: '',
-        password: ''
-      });
-    } catch (error) {
-      console.error(error);
-    }
+  const validationSchema = Yup.object({
+    
+    empid: Yup.string().required('Employee ID Required'),
+    firstName: Yup.string().required('First Name Required'),
+    lastName: Yup.string().required('Last Name Required'),
+    age: Yup.number().required('Age is required'),
+    visits: Yup.number().required('Visits is required'),
+    progress: Yup.number().required('Progress is required'),
+    status: Yup.string().required('Please select Status'),
+  });
+
+  const handleSubmit = (values, { setSubmitting }) => {
+    setTimeout(() => {
+      console.log(values);
+      setSubmitting(false);
+    }, 500);
   };
 
   return (
-    <div>
-      <h1>Registration Form</h1>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="name">Name:</label>
-          <input type="text" id="name" name="name" value={formData.name} onChange={handleChange} />
-        </div>
-        <div>
-          <label htmlFor="email">Email:</label>
-          <input type="email" id="email" name="email" value={formData.email} onChange={handleChange} />
-        </div>
-        <div>
-          <label htmlFor="password">Password:</label>
-          <input type="password" id="password" name="password" value={formData.password} onChange={handleChange} />
-        </div>
-        <button type="submit">Register</button>
-      </form>
-    </div>
-  );
-}
+    <div className="Mainbox1">
+    <div className="registration-form">
+      <h1>Register Employee</h1>
+      <Formik
+        initialValues={initialValues}
+        validationSchema={validationSchema}
+        onSubmit={handleSubmit}
+      >
+        {({ isSubmitting }) => (
+          
+          <form>
+          
+          <div className="form-group">
+              <label htmlFor="empid">Employee ID:</label>
+              <Field type="text" id="empid" name="empid" />
+              <ErrorMessage name="empid" component="div" className="error" />
+            </div>
 
-export default RegistrationForm;
+            <div className="form-group">
+              <label htmlFor="firstName">First Name:</label>
+              <Field type="text" id="firstName" name="firstName" />
+              <ErrorMessage name="firstName" component="div" className="error" />
+            </div>
+
+            <div className="form-group">
+              <label htmlFor="lastName">Last Name:</label>
+              <Field type="text" id="lastName" name="lastName" />
+              <ErrorMessage name="lastName" component="div" className="error" />
+            </div>
+
+            <div className="form-group">
+              <label htmlFor="age">Age:</label>
+              <Field type="number" id="age" name="age" />
+              <ErrorMessage name="age" component="div" className="error" />
+            </div>
+
+            <div className="form-group">
+              <label htmlFor="visits">Visits:</label>
+              <Field type="number" id="visits" name="visits" />
+              <ErrorMessage name="visits" component="div" className="error" />
+            </div>
+
+            <div className="form-group">
+              <label htmlFor="progress">Progress:</label>
+              <Field type="number" id="progress" name="progress" />
+              <ErrorMessage name="progress" component="div" className="error" />
+            </div>
+
+            <div className='Dropdown'>
+          <label htmlFor='status'>Status:</label>
+          <Field name="status" as="select">
+          <option value="option1">Select Status</option>
+          <option value="option2">Single</option>
+          <option value="option3">Married</option>
+          <option value="option4">Complicated</option>
+          </Field>
+          <ErrorMessage name="status" component="div" className="error" />
+        </div>
+
+            <button type="submit" disabled={isSubmitting}>
+              {isSubmitting ? 'Submitting...' : 'Submit'}
+            </button>
+          </form>
+          
+        )}
+      </Formik>
+    </div>
+    </div>
+    
+  );
+};
+
+export default AddUserForm;
